1560fd87337017f6233caa490d4cfcfebcf4238f
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-11-12 19:13:33 -0300

    Add files via upload

---
+++ Exerc_09/Exerc_09/Exerc_09/Calculo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_09
+{
+    class Calculo
+    {
+        #region atributos
+        private int num1;
+        private int num2;
+        #endregion
+
+        #region construtores
+        public Calculo()
+        {
+            this.num1 = 0;
+            this.num2 = 0;
+        }
+
+        public Calculo(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        #endregion
+
+        #region métodos de interface (Setter's & Getter's)
+        public void setNum1(int num1)
+        {
+            this.num1 = num1;
+        }
+
+        public void setNum2(int num2)
+        {
+            this.num2 = num2;
+        }
+
+        public int getNum1()
+        {
+            return this.num1;
+        }
+
+        public int getNum2()
+        {
+            return this.num2;
+        }
+        #endregion
+
+        #region métodos funcionais
+        public void calcular()
+        {
+            if (this.num1 == this.num2)
+            {
+                Console.Write("Os números digitados são iguais");
+            }
+
+            else
+            {
+                if (this.num1 > this.num2)
+                {
+                    Console.Write("O número {0} é o maior entre eles", getNum1());
+                }
+                else
+                {
+                    Console.Write("O número {0} é o maior entre eles", getNum2());
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Exerc_09/Exerc_09/Exerc_09/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exerc_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region declarações & instâncias
+            Calculo s;
+            s = new Calculo();
+            #endregion
+
+            #region Introducao
+            Console.Write("Descubra qual o Maior Numero, ou se eles sao iguais");
+            Console.ReadLine();
+            #endregion
+
+            #region entrada
+            Console.Write("Informe o 1° numero: ");
+            s.setNum1(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o 2° numero: ");
+            s.setNum2(int.Parse(Console.ReadLine()));
+            #endregion
+
+            #region processo
+            s.calcular();
+            #endregion
+
+        }
+    }
+}
